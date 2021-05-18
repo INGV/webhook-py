35df7b786c19524563e58036ea5f79379cedefda
Sergio Bruni <sergio.bruni@ingv.it>
2021-05-18 15:08:34 +0200

    first commit

---
+++ server/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Run below commands from manage.py to initialize db and have some default data.
+uwsgi --ini /etc/uwsgi.ini
---
+++ server/main/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""Angular-Flask-Docker-Skeleton
+
+    Main application package
+
+"""
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from server.main.utils.common import register_blueprints
+from server.settings import config
+
+# instantiate the db
+db = SQLAlchemy()
+
+
+def create_app(config_type, package_name, package_path):
+    app = Flask(__name__, instance_relative_config=True)
+    # set default config
+    # app_settings = os.getenv('APP_DEV_SETTINGS')
+    app_settings = config[config_type]
+    app.config.from_object(app_settings)
+
+    # sergio
+    #webserver.init()
+
+    # instantiate the db
+    db.init_app(app)
+
+    # Access config variables as: app.config['DEBUG']
+    # Register all api blueprints found in the application
+    print (package_path)
+    register_blueprints(app, package_name, package_path)
+
+    return app
