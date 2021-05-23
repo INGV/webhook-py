69edd0dac70116bc111c6b929a515be35ef341e5
Sergio Bruni <sergio.bruni@ingv.it>
2021-05-23 18:51:56 +0200

    ngnix.conf modified

--- server/devel-server.py
+++ server/devel-server.py
@@ -1,6 +1,6 @@
 # this script runs the Flask Development Server
 # Not used in production !!!
 from server.main.api import create_app_blueprint
-#if __name__ == '__main__':
+
 application = create_app_blueprint('development')
 application.run(debug=True, use_debugger=False, use_reloader=False, port=4300, host='0.0.0.0')
